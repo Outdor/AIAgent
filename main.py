4e8eb66ae433cac081ed9f0985eca4ee153e7f60
Outdor <outdor@outdorsrealm.net>
2026-08-05 12:18:14 -0700

    U08:CH1:L6 - Multiple Messages

--- main.py
+++ main.py
@@ -10,42 +10,46 @@ import argparse
 from dotenv import load_dotenv
 from openai import OpenAI
 
-# Load dotenv.
-load_dotenv()
-
-# Load API Key.
-try:
-    api_key = os.environ.get("OPENROUTER_API_KEY")
-except:
-   raise RuntimeError("API Key not found")
-
-
-# Point OpenAI at the OpenRouter url.
-client = OpenAI(
-    base_url="https://openrouter.ai/api/v1",
-    api_key=api_key,
-)
-
-# Create a parser object.
-parser = argparse.ArgumentParser(description="Chatbot")
-parser.add_argument("user_prompt", type=str, help="User prompt")
-args = parser.parse_args()
-# Now we can access `args.user_prompt`
-
-
-# Use the client.chat.completions.create() method to get a response from the model.
-model = "openrouter/free"
-messages = [
-    {
-        "role": "user",
-        "content": args.user_prompt,
-    }
-]
-response = client.chat.completions.create(model=model, messages=messages)
-
 
 ### MAIN ###
 def main():
+    # Load dotenv.
+    load_dotenv()
+
+
+    # Load API Key.
+    try:
+        api_key = os.environ.get("OPENROUTER_API_KEY")
+    except:
+       raise RuntimeError("API Key not found")
+
+
+    # Point OpenAI at the OpenRouter url.
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=api_key,
+    )
+
+
+    # Create a parser object and parse th euser input.
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    # Now we can access `args.user_prompt`
+
+
+    # Define the OpenRouter model to use.
+    model = "openrouter/free"
+
+    # Create a list of messages,
+    messages = [
+        {"role": "user", "content": args.user_prompt},
+    ]
+
+    # Use the client.chat.completions.create() method to get a response from the model.
+    response = client.chat.completions.create(model=model, messages=messages)
+
+    # Ouput response to the user.
     if response.usage is not None:
         print(f"User Prompt: {messages[0]['content']}")
         print(f"Prompt tokens: {response.usage.prompt_tokens}")
