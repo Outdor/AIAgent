00a68657bafc36a91aac4913d2887801a391d1f1
Outdor <outdor@outdorsrealm.net>
2026-08-05 20:19:36 -0700

    U08:CH2:L3 - List Files

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -4,9 +4,10 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
     working_dir_abs = os.path.abspath(working_directory)
     target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
 
-    # Will be True or False
+    # Check the validity of the target directory
     valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
 
+    # Catch and return any erros.
     try:
         if valid_target_dir == False:
             return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
@@ -14,6 +15,12 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
             if os.path.isdir(target_dir) == False:
                 return f'Error: "{directory}" is not a directory'
             else:
-                return f'Success: "{directory}" is within the working directory'
+                # BUild a return string.
+                result = ""
+
+                # if it is a valid procede.
+                for each in os.listdir(target_dir):
+                    result += f"  - {os.path.join(target_dir, each)}: file_size={os.path.getsize(os.path.join(target_dir, each))}, is_dir={os.path.isdir(os.path.join(target_dir, each))}\n"
+                return result
     except Exception as e:
         return f"Error: {e}"
--- test_get_files_info.py
+++ test_get_files_info.py
@@ -1,6 +1,11 @@
 from functions.get_files_info import get_files_info
 
-print(get_files_info("calculator", "."))
-print(get_files_info("calculator", "/bin"))
-print(get_files_info("calculator", "../"))
-print(get_files_info("calculator", "main.py"))
+def print_results(working_directory: str, directory: str = "."):
+    result = get_files_info(working_directory, directory)
+    print(f'Result for "{directory}":')
+    print(result)
+
+print_results("calculator", ".")
+print_results("calculator", "pkg")
+print_results("calculator", "/bin")
+print_results("calculator", "../")
