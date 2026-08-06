6174be3b9d34a56ecd891e9b0440719bf632d20c
Outdor <outdor@outdorsrealm.net>
2026-08-05 20:40:53 -0700

    U08:CH2:L4 - Get Files Content

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -10,7 +10,10 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
     # Catch and return any erros.
     try:
         if valid_target_dir == False:
-            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+            return (
+                f'Error: Cannot list "{directory}" as ' +
+                'it is outside the permitted working directory'
+            )
         elif valid_target_dir == True:
             if os.path.isdir(target_dir) == False:
                 return f'Error: "{directory}" is not a directory'
@@ -20,7 +23,11 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
 
                 # if it is a valid procede.
                 for each in os.listdir(target_dir):
-                    result += f"  - {os.path.join(target_dir, each)}: file_size={os.path.getsize(os.path.join(target_dir, each))}, is_dir={os.path.isdir(os.path.join(target_dir, each))}\n"
+                    result += (
+                        f"  - {os.path.join(target_dir, each)}: " +
+                        f"file_size={os.path.getsize(os.path.join(target_dir, each))}, " +
+                        f"is_dir={os.path.isdir(os.path.join(target_dir, each))}\n"
+                    )
                 return result
     except Exception as e:
         return f"Error: {e}"
